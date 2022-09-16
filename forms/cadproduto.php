91052c00402d1c47a14f7c4db105ab4196c253b2
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-16 15:11:15 -0300

    faltando apenas a refaturação da tela de cadastro de usuário

--- forms/cadproduto.php
+++ forms/cadproduto.php
@@ -1,73 +1,94 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>CADASTRO PRODUTO</title>
-    <style>
-      *{
-        margin: 0;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-      }
-    </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <title>CADASTRO PRODUTO</title>
+  <style>
+    * {
+      margin: 0;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+  </style>
 </head>
+
 <body>
-    
-        <form action="" method="post">
-                
-                <input type="text" class="input-field" name="produto" value=""/>
-                
-                    
-                <input type="text" class="input-field" name="cat" value=""/>
-               
-                <input type="number" class="input-field" name="valor" maxlength="11" value=""/>
-                
-                
-                <input type="number" class="input-field" name="estoque" maxlength="11" value=""/>
-                
-                <input type="number" class="input-field" name="empresa" maxlength="8"  value=""/>
-                
-                <input type="text" class="input-field" name="cnpj"  value=""/>
-                
-                <input type="submit" value="Submit" />
-                
-            </form>
+<header>
+  <h1>Cadastro de produto</h1>
+</header>
+  <form action="" method="post">
+    <div class="linha">
+      <p>produto: <strong>*</strong></p>
+      <input type="text" name="produto" required/>
     </div>
-<?php
+    <div class="linha">
+      <p>Categoria: <strong>*</strong></p>
+      <input type="text" name="cat" required/>
+    </div>
+    <div class="linha">
+      <p>Valor: <strong>*</strong></p>
+      <input type="number" name="valor" maxlength="11" required/>
+    </div>
+    <div class="linha">
+      <p>Quantia em estoque: <strong>*</strong></p>
+      <input type="number" name="estoque" maxlength="11"  required/>
+    </div>
+    <div class="linha">
+      <p>Empresa: <strong>*</strong></p>
+      <input type="text" name="empresa" maxlength="8" required/>
+    </div>
+    <div class="linha">
+      <p>CNPJ: <strong>*</strong></p>
+      <input type="number" name="cnpj" required/>
+    </div>
+    <div class="linha">
+      <input type="submit" value="Submit" id="btn"/>
+    </div>
 
-    if(!empty($_POST))
-    {
-        $produto = $_POST['produto'];
-        $cat = $_POST['cat'];
-        $valor = $_POST['valor'];
-        $estoque = $_POST['estoque'];
-        $empresa = $_POST['empresa'];
-        $cnpj = $_POST['cnpj'];
 
 
-        include_once('conexao.php');
 
-        try {
-            $stmt = $conn->prepare("INSERT INTO tb_produto (nm_produto, nm_categoria, vl_produto, qt_produto, nm_empresa, nr_cnpj)
+
+
+
+
+
+  </form>
+  </div>
+  <?php
+
+  if (!empty($_POST)) {
+    $produto = $_POST['produto'];
+    $cat = $_POST['cat'];
+    $valor = $_POST['valor'];
+    $estoque = $_POST['estoque'];
+    $empresa = $_POST['empresa'];
+    $cnpj = $_POST['cnpj'];
+
+
+    include_once('conexao.php');
+
+    try {
+      $stmt = $conn->prepare("INSERT INTO tb_produto (nm_produto, nm_categoria, vl_produto, qt_produto, nm_empresa, nr_cnpj)
             VALUES (:prod, :cat, :valor, :estoque, :empresa, :cnpj)");
 
-            $stmt->bindParam(':prod', $produto);
-            $stmt->bindParam(':cat', $cat);
-            $stmt->bindParam(':valor', $valor);
-            $stmt->bindParam(':estoque', $estoque);
-            $stmt->bindParam(':empresa', $empresa);
-            $stmt->bindParam(':cnpj', $cnpj);
-            
-            $stmt->execute();
+      $stmt->bindParam(':prod', $produto);
+      $stmt->bindParam(':cat', $cat);
+      $stmt->bindParam(':valor', $valor);
+      $stmt->bindParam(':estoque', $estoque);
+      $stmt->bindParam(':empresa', $empresa);
+      $stmt->bindParam(':cnpj', $cnpj);
 
-            echo "<script>alert('Cadastrado com Sucesso');</script>";
-      
-          } catch(PDOException $e) {
-            echo "Erro ao cadastrar: " . $e->getMessage();
-          }
-          $connection = null;
+      $stmt->execute();
+
+      echo "<script>alert('Cadastrado com Sucesso');</script>";
+    } catch (PDOException $e) {
+      echo "Erro ao cadastrar: " . $e->getMessage();
     }
+    $connection = null;
+  }
 
-?> 
+  ?>
--- forms/index.php
+++ forms/index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistema Comercial &copy;</title>
     <link rel="stylesheet" href="./public/css/home.css">
 </head>
 <body>
        
-    
+    <!-- baixe para testar http://www.usbwebserver.net/downloads/USBWebserver%20v8.6.zip -->
     <main> 
         <h1>Firma do Toguro</h1>
         <div class="flexpag">
@@ -32,6 +32,9 @@
             <div class="box">
                 Cadastrar produto
                 <a href="cadproduto.php" class="btn">Cadastrar</a>
+            </div>
+            <div class="box">
+                <a href="conexao.php">Conectar com o banco</a>
             </div>
         </div>
     </main>
